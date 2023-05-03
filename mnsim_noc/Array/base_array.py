45f8054129618c141f51a6663fe724822b7cbc9c
ILTShade <sunhanbo123@163.com>
2023-05-03 10:35:25 +0000

    modify for loop in while loop   * get comm_id_list and tile_id_list   * add sorted list for faster next time   * comment useless outputs

--- mnsim_noc/Array/base_array.py
+++ mnsim_noc/Array/base_array.py
@@ -8,7 +8,8 @@
     2021/10/08 18:21
 """
 import time
-import numpy as np
+# import numpy as np
+import bisect
 from mnsim_noc.utils.component import Component
 from mnsim_noc.Strategy.mapping import Mapping
 from mnsim_noc.Strategy.schedule import Schedule
@@ -132,41 +133,59 @@ class BaseArray(Component):
             for tile_id, tile in enumerate(tile_list):
                 if tile.input_buffer.start_flag:
                     tile_update_flag[tile_id] = True
+            sorted_finish_time_list = []
             start_time = time.time()
             while True:
                 # running the data in the optimal order
                 # first, update part of the communication which can be done in current time
-                for comm_id, comm_end_time in enumerate(communication_end_time_list):
-                    if comm_end_time <= current_time:
-                        # 1.1 update the communication, and end time
-                        communication_list[comm_id].update(current_time)
-                        communication_end_time_list[comm_id] = \
-                            communication_list[comm_id].get_communication_end_time()
-                        # 1.2 add to the schedule flag list
-                        communication_schedule_flag[comm_id] = True
-                        # 1.3 set successors tile update flag
-                        for tile_id in comm_successors[comm_id]:
-                            tile_update_flag[tile_id] = True
-                # second, update part of the tile which can be done in current time
-                for tile_id, tile_end_time in enumerate(tile_end_time_list):
-                    if tile_end_time <= current_time:
-                        # 2.1 add to the update flag list
+                comm_id_list = [
+                    i for i, comm_end_time in enumerate(communication_end_time_list) \
+                    if comm_end_time <= current_time
+                ]
+                for comm_id in comm_id_list:
+                    # 1.1 update the communication, and end time
+                    communication_list[comm_id].update(current_time)
+                    communication_end_time_list[comm_id] = float("inf")
+                    # 1.2 add to the schedule flag list
+                    communication_schedule_flag[comm_id] = True
+                    # 1.3 set successors tile update flag
+                    for tile_id in comm_successors[comm_id]:
                         tile_update_flag[tile_id] = True
-                        # 2.2 set predecessors and successors comm schedule flag
-                        for comm_id in tile_predecessors[tile_id]:
-                            communication_schedule_flag[comm_id] = True
-                        for comm_id in tile_successors[tile_id]:
-                            communication_schedule_flag[comm_id] = True
+                    # 1.4 pop one element from the sorted finish time list
+                    sorted_finish_time_list.pop(0)
+                # second, update part of the tile which can be done in current time
+                tile_id_list = [
+                    i for i, tile_end_time in enumerate(tile_end_time_list) \
+                    if tile_end_time <= current_time
+                ]
+                for tile_id in tile_id_list:
+                    # 2.1 add to the update flag list
+                    tile_update_flag[tile_id] = True
+                    # 2.2 set predecessors and successors comm schedule flag
+                    for comm_id in tile_predecessors[tile_id]:
+                        communication_schedule_flag[comm_id] = True
+                    for comm_id in tile_successors[tile_id]:
+                        communication_schedule_flag[comm_id] = True
+                    # 2.3 pop one element from the sorted finish time list
+                    sorted_finish_time_list.pop(0)
                 # third, update based on the update flag
                 # achieve no redundant update
-                for tile_id, tile_update in enumerate(tile_update_flag):
-                    if tile_update:
-                        # 3.1 update the tile
-                        tile_list[tile_id].update(current_time)
-                        # 3.2 update the end tile
-                        tile_end_time_list[tile_id] = tile_list[tile_id].get_computation_end_time()
-                        # 3.3 update the update flag, only update only state changes
-                        tile_update_flag[tile_id] = False
+                tile_id_list = [i for i, flag in enumerate(tile_update_flag) if flag]
+                for tile_id in tile_id_list:
+                    before_finish_time = tile_end_time_list[tile_id]
+                    # 3.1 update the tile
+                    tile_list[tile_id].update(current_time)
+                    # 3.2 update the end tile
+                    tile_end_time_list[tile_id] = tile_list[tile_id].get_computation_end_time()
+                    # 3.3 update the update flag, only update only state changes
+                    tile_update_flag[tile_id] = False
+                    # 3.4 add to the sorted finish time list
+                    after_finish_time = tile_end_time_list[tile_id]
+                    if after_finish_time < float("inf") and \
+                        after_finish_time != before_finish_time:
+                        bisect.insort(
+                            sorted_finish_time_list, tile_end_time_list[tile_id]
+                        )
 
                 # schedule for the path
                 task_comm_id_list = \
@@ -179,14 +198,20 @@ class BaseArray(Component):
                     # 4.2 set the update flag of the tile successors as True
                     for tile_id in comm_predecessors[comm_id]:
                         tile_update_flag[tile_id] = True
+                    # 4.3 add to the sorted finish time list
+                    bisect.insort(
+                        sorted_finish_time_list, communication_end_time_list[comm_id]
+                    )
                 # 4.3 set the communication schedule flag as False if in running or done
-                for comm_id, comm in enumerate(communication_list):
+                comm_id_list = [i for i, flag in enumerate(communication_schedule_flag) if flag]
+                for comm_id in comm_id_list:
+                    comm = communication_list[comm_id]
                     if comm.running_state or \
                         comm.number_done_communication == comm.number_total_communication:
                         communication_schedule_flag[comm_id] = False
 
                 # get next time
-                next_time = min(min(tile_end_time_list), min(communication_end_time_list))
+                next_time = sorted_finish_time_list[0] if sorted_finish_time_list else float("inf")
                 # check if the simulation is over
                 assert next_time > current_time
                 current_time = next_time
@@ -198,26 +223,29 @@ class BaseArray(Component):
             # check if the simulation is over
             self.check_finish(tile_list, communication_list, wire_net)
             # log info
-            self.logger.info(f"For the {_}th: {fitness}, {time_point_list[-1]/1e6:.3f}")
+            self.logger.info(
+                f"For the {_}th: {fitness}, \033[1;31;40m{time_point_list[-1]/1e6:.3f}\033[0m" + \
+                f" ms, {end_time - start_time:.3f} seconds"
+            )
             # log communication amount
-            energy_bit = 5.445 + 0.43 # pJ / bit
-            total_transfer_bits = wire_net.get_communication_amounts()/1e6 # M bits
-            self.logger.info(f"Communication amounts: {total_transfer_bits:.2f} M bits")
-            self.logger.info(f"Energy consumption: {total_transfer_bits*energy_bit/1e3:.3f} mJ")
-            # get running rate for all wires
-            horizontal_rate, vertical_rate = wire_net.get_running_rate(time_point_list[-1])
-            rate_sum = np.sum(horizontal_rate) + np.sum(vertical_rate)
-            self.logger.info(f"total running rate: {rate_sum}")
+            # energy_bit = 5.445 + 0.43 # pJ / bit
+            # total_transfer_bits = wire_net.get_communication_amounts()/1e6 # M bits
+            # self.logger.info(f"Communication amounts: {total_transfer_bits:.2f} M bits")
+            # self.logger.info(f"Energy consumption: {total_transfer_bits*energy_bit/1e3:.3f} mJ")
+            # # get running rate for all wires
+            # horizontal_rate, vertical_rate = wire_net.get_running_rate(time_point_list[-1])
+            # rate_sum = np.sum(horizontal_rate) + np.sum(vertical_rate)
+            # self.logger.info(f"total running rate: {rate_sum}")
         # save for the output
-        output_list = np.array(output_list)
-        file_name = f"output_info_{self.task_name_label}.txt"
-        np.savetxt(file_name, output_list, fmt="%.3f")
-        self.logger.info(f"The output info is saved in {file_name}")
+        # output_list = np.array(output_list)
+        # file_name = f"output_info_{self.task_name_label}.txt"
+        # np.savetxt(file_name, output_list, fmt="%.3f")
+        # self.logger.info(f"The output info is saved in {file_name}")
         # get min index
-        min_index = np.argsort(output_list[:, 1])[0]
-        self.logger.info(
-            f"The min index is {min_index}, and latency is {output_list[min_index, 1]}"
-        )
+        # min_index = np.argsort(output_list[:, 1])[0]
+        # self.logger.info(
+        #     f"The min index is {min_index}, and latency is {output_list[min_index, 1]}"
+        # )
 
     def check_finish(self, tile_list, communication_list, wire_net):
         """
